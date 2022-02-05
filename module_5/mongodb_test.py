3db8d11b0d38cbe23b1f2f5b37e5579be8f97298
harrisonBirkner <harrison.birkner@gmail.com>
2022-02-05 11:34:22 -0600

    Added directory, moved file in, modified output and added comments

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,21 @@
+# Harrison Birkner, module 5.2 assignment, 2/3/2022 
+
+# The purpose of this program is to attempt to connect to my pytech
+# MongoDB database and print a list of collections in the database.
+
+from pymongo import MongoClient
+import certifi
+
+def main():
+    url = "mongodb+srv://admin:admin@cluster0.jancr.mongodb.net/pytech?retryWrites=true&w=majority"
+    client = MongoClient(url,tlsCAFile=certifi.where())
+    db = client.pytech
+
+    try:
+        print("-- Pytech Collection List --")
+        print(db.list_collection_names())
+        input("\nEnd of program, press any key to exit...")
+    except Exception as e:
+        print("Error connecting to database: ", e)
+    
+main()
