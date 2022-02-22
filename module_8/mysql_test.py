632d8696cbaa7323a1d15b66deeebc63ac4dab88
harrisonBirkner <harrison.birkner@gmail.com>
2022-02-21 18:03:08 -0600

    Created mysql_test file & module_8 directory

---
+++ module_8/mysql_test.py
@@ -0,0 +1,37 @@
+#Harrison Birkner, 02/21/2022, Module 8.2 Assignment
+
+#The purpose of this program is to attempt to connect to the pytest mysql database
+
+import mysql.connector
+from mysql.connector import errorcode
+
+def main():
+    config = {
+        "user": "pysports_user",
+        "password": "MySQL8IsGreat!",
+        "host": "127.0.0.1",
+        "database": "pysports",
+        "raise_on_warnings": True
+    }
+
+    try:
+        db = mysql.connector.connect(**config) 
+
+        print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+        input("\n\n  Press any key to continue...")
+
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("  The supplied username or password are invalid")
+
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("  The specified database does not exist")
+
+        else:
+            print(err)
+
+    finally:
+        db.close()
+
+main()
